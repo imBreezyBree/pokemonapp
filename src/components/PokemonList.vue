bfdb046a0ec48cfcf138f55278284366fa1967b8
adityar15 <adityask2194@gmail.com>
2023-02-02 21:40:09 +0000

    pokedex app

---
+++ src/components/PokemonList.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <Pokemon :pokemon="pokemon" v-for="(pokemon, index) in pokemons" :key="index" />
+    </div>
+</template>
+
+<script setup>
+import pokemons from "@/assets/sampledataset.json"
+import Pokemon from "./Pokemon.vue"
+
+// mount and then make api call
+</script>
--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -6,6 +6,6 @@
 </template>
 
 <script setup>
-
+// HomeView.vue
 </script>
 
